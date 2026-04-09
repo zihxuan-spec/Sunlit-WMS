ecd947870da03b1d8122d05c3e76107f3712bee6
Dylan <zih.xuan@icloud.com>
2026-04-09 14:31:21 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 請把 wms-project 換成你的 GitHub Repository 名稱！
-  base: '/Sunlit-WMS/' 
+  // 把原本的 base: '/wms-project/' 整行刪掉！
 })
